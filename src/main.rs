3b1718b41090ce8d3fabdfc4cc3dbf96bd841cdf
Eduardo Ferreira da Silva <edugera47@gmail.com>
2026-04-26 17:27:38 -0300

    Implements notional calculator closes #2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+fn calculate_notional(volume_mwh: f64, price_mwh: f64) -> f64 {
+    volume_mwh * price_mwh
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let volume = 100.0;
+    let price = 235.0;
+
+    let notional = calculate_notional(volume, price);
+
+    println!("Energy Futures Calculator");
+    println!("Volume: {} MWh", volume);
+    println!("Price: R$ {}", price);
+    println!("Contract Notional: R$ {}", notional);
 }
